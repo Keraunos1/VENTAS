47e1b22884b469c42f3c413dc426d8da8939e1a8
Larry Rimache López <larry_rimache_lopez_20@hotmail.com>
2026-03-25 13:25:06 -0500

    Conexión y modelo Category agregado

---
+++ src/main/java/practice/app/ventas/Category.java
@@ -0,0 +1,19 @@
+package practice.app.ventas;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="categories")
+public class Category {
+    @Id
+    private Integer categoryid;
+    @Column(name="categoryname")
+    private String categoryname;
+    @Column(name="description")
+    private String description;
+}
