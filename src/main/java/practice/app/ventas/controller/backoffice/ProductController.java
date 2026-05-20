9f6cca0b6a64860f8cb2be0de06346bd33b18ed1
Larry Rimache López <larry_rimache_lopez_20@hotmail.com>
2026-05-19 19:13:03 -0500

    controller product terminado

--- src/main/java/practice/app/ventas/controller/backoffice/ProductController.java
+++ src/main/java/practice/app/ventas/controller/backoffice/ProductController.java
@@ -1,12 +1,15 @@
 package practice.app.ventas.controller.backoffice;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import practice.app.ventas.model.bd.Category;
 import practice.app.ventas.model.bd.Product;
+import practice.app.ventas.model.bd.Supplier;
+import practice.app.ventas.model.dto.request.ProductRequest;
+import practice.app.ventas.model.dto.response.ProductResponse;
 import practice.app.ventas.service.IProductService;
 
 import java.util.List;
@@ -16,17 +19,42 @@ import java.util.List;
 @AllArgsConstructor
 public class ProductController {
     private IProductService iProductService;
+
     @GetMapping("")
-    public String formProduct(Model model){
+    public String formProduct(Model model) {
         model.addAttribute("listproduct", iProductService.listProducts());
         return "backoffice/product/formproduct";
     }
+
     @GetMapping("/list")
     @ResponseBody
-    public List<Product> listProducts(){
+    public List<Product> listProducts() {
         return iProductService.listProducts();
     }
-    public void registerProduct(){
-
+    @PostMapping("/register")
+    @ResponseBody
+    public ProductResponse registerProduct(@RequestBody  ProductRequest productRequest) {
+        String mensaje = "Se registro el producto";
+        boolean respuesta = true;
+        try {
+            Product product = new Product();
+            if(productRequest.getProductid() > 0){
+                product.setProductid(productRequest.getProductid());
+            }
+            product.setProductname(productRequest.getProductname());
+            product.setUnitprice(productRequest.getUnitprice());
+            product.setDiscontinued(productRequest.getDiscontinued());
+            Category category = new Category();
+            category.setCategoryid(productRequest.getCategoryid());
+            product.setCategory(category);
+            Supplier supplier = new Supplier();
+            supplier.setSupplierid(productRequest.getSupplierid());
+            product.setSupplier(supplier);
+            iProductService.registerProduct(product);
+        } catch (Exception ex) {
+                mensaje = "Producto no registrado, error en la BD";
+                respuesta = false;
+        }
+        return ProductResponse.builder().mensaje(mensaje).respuesta(respuesta).build();
     }
 }
